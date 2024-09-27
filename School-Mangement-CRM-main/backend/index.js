d1eec7f5b12bfa33db0317da051a6cef4122aa68
ShubhamChoudharyShubh <shubhamchoudharyshubh@gmail.com>
2024-09-28 02:30:02 +0530

    code update

--- School-Mangement-CRM-main/backend/index.js
+++ School-Mangement-CRM-main/backend/index.js
@@ -4,12 +4,9 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const app = express()
 const Routes = require("./routes/route.js")
-
 const PORT = process.env.PORT || 5000
-const db = process.env.db
 
 dotenv.config();
-
 
 app.use(express.json({ limit: '10mb' }))
 app.use(cors({
